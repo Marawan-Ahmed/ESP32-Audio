039f3cdbcae066b69d6524b61247911562438572
Marawan-Ahmed <memoahmedrabee@yahoo.com>
2022-12-24 01:06:34 +0200

    final

--- YesNo-Speech-Recognition-FFT-master/yesno.py
+++ YesNo-Speech-Recognition-FFT-master/yesno.py
@@ -3,6 +3,7 @@ from scipy.fftpack import fft
 import pyaudio
 import wave
 import numpy as np
+from numpy.linalg import norm
 import matplotlib.pyplot as plt
 
 import time
@@ -16,43 +17,17 @@ WAVE_OUTPUT_FILENAME = "file.wav"  # file name
 FREQBANDS = 128
 
 refrence_records = []
+sample_per_record = 10
 audio = pyaudio.PyAudio()
-for record in ["1","2","3","4","5","6","7","8","9","10"]:
-    # print("Start recording"+record)
-    # stream = audio.open(
-    #     format=FORMAT,
-    #     channels=CHANNELS,
-    #     rate=RATE,
-    #     input=True,
-    #     frames_per_buffer=CHUNK
-    # )
 
-    # frames = []
-
-    # for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    #     data = stream.read(CHUNK)
-    #     frames.append(data)
-    # print("finished recording")
-
-    # # stop Recording
-    # stream.stop_stream()
-    # stream.close()
-
-    # # storing voice
-    # waveFile = wave.open(record+".wav", 'wb')
-    # waveFile.setnchannels(CHANNELS)
-    # waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-    # waveFile.setframerate(RATE)
-    # waveFile.writeframes(b''.join(frames))
-    # waveFile.close()
-
-    rate, data = wav.read(record+".wav")
+def get_fft(Sample):
+    rate, data = wav.read(Sample)
 
     pow_audio_signal = data / np.power(2, 15)
     pow_audio_signal = pow_audio_signal [:100]
 
     sig_length = len(pow_audio_signal)
-    half_length = np.ceil((sig_length + 1) / 2.0).astype(np.int)
+    # half_length = np.ceil((sig_length + 1) / 2.0).astype(np.int)
 
     vc = np.fft.fft(data)
     abs_vc = np.absolute(vc)
@@ -62,8 +37,6 @@ for record in ["1","2","3","4","5","6","7","8","9","10"]:
 
     total_energy = np.sum(abs_vc)
 
-
-
     for freq in range(len(abs_vc)) :
         if freq < 8000:
             freqband = int(freq/(8000/FREQBANDS))
@@ -72,20 +45,11 @@ for record in ["1","2","3","4","5","6","7","8","9","10"]:
     for i in range(FREQBANDS):
         freqspectrm[i] /= total_energy
     
-    refrence_records.append(freqspectrm)
+    return (freqspectrm)
 
-    # plotting
-#     plt.title("Line graph")
-#     plt.xlabel("Freq band")
-#     plt.ylabel("Energy")
-#     plt.plot(np.arange(0,FREQBANDS), freqspectrm)
-
-# plt.show()
-# print(refrence_records)
-
-while True:
+def record_sample(output_file):
     # start Recording
-    print("Start recording")
+    print("Start recording " + output_file )
     stream = audio.open(
         format=FORMAT,
         channels=CHANNELS,
@@ -106,53 +70,42 @@ while True:
     stream.close()
 
     # storing voice
-    waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    waveFile = wave.open(output_file, 'wb')
     waveFile.setnchannels(CHANNELS)
     waveFile.setsampwidth(audio.get_sample_size(FORMAT))
     waveFile.setframerate(RATE)
     waveFile.writeframes(b''.join(frames))
     waveFile.close()
 
-    rate, data = wav.read(WAVE_OUTPUT_FILENAME)
-    # rate, data = wav.read("4.wav")
+def get_simlarity(vect1 , vect2):
+    similarity_factor = np.sum(np.square(np.subtract(vect1,vect2)))
+    # similarity_factor = np.dot(vect1,vect2)/(norm(vect1)*norm(vect2))
+    return similarity_factor
 
-    pow_audio_signal = data / np.power(2, 15)
-    pow_audio_signal = pow_audio_signal [:100]
+for record in ["1","2","3","4","5","6","7","8","9","10"]:   
+    temp_arr = []
+    for sample in range (sample_per_record):
+    # record_sample(record+".wav")
 
-    sig_length = len(pow_audio_signal)
-    half_length = np.ceil((sig_length + 1) / 2.0).astype(np.int)
+        temp_vect = get_fft(record+"_"+str(sample)+".wav")
+        temp_arr.append(temp_vect)
+    # plt.plot(np.arange(0,len(temp)), temp)
+    refrence_records.append(temp_arr)
 
-    vc = np.fft.fft(data)
-    abs_vc = np.absolute(vc)
-
-    # time_axis = 1000 * np.arange(0, len(pow_audio_signal), 1) / float(rate)
-    # plt.plot(np.arange(0,len(abs_vc)), abs_vc)
-    # plt.show()
-
-    abs_vc = abs_vc[0:int(len(abs_vc)/2)]/len(pow_audio_signal)
-
-    freqspectrm = [0] * FREQBANDS 
-
-    total_energy = np.sum(abs_vc)
+# plt.show()
 
 
-
-    for freq in range(len(abs_vc)) :
-        if freq < 8000:
-            freqband = int(freq/(8000/FREQBANDS))
-            freqspectrm[freqband] += abs_vc[freq]
-            
-    for i in range(FREQBANDS):
-        freqspectrm[i] /= total_energy
+while True:
+    record_sample(WAVE_OUTPUT_FILENAME)
+    sample_vector = get_fft(WAVE_OUTPUT_FILENAME)
 
     probabilty_vector = [0]*len(refrence_records)
-    for i in range(len(refrence_records)):
-        # probabilty_vector[i] = np.linalg.norm(freqspectrm - refrence_records[i])
-        for j in range(len(freqspectrm)):
-            probabilty_vector[i] += (freqspectrm[j] - refrence_records[i][j])**2
+    for record in range(len(refrence_records)):
+        for sample in range(sample_per_record):
+            probabilty_vector[record] += get_simlarity(sample_vector, refrence_records[record][sample])
 
-    probabilty_vector /= np.max(probabilty_vector)
-    probabilty_vector = 1 - probabilty_vector
+    # probabilty_vector /= (np.max(probabilty_vector) * sample_per_record)
+    # probabilty_vector = 1 - probabilty_vector
 
 
     max = probabilty_vector[0]
@@ -161,7 +114,7 @@ while True:
         if probabilty_vector[i] > max:
             max = probabilty_vector[i]
             index = i
-    if max > 0.7:
+    if max > 5:
         print(index+1)
 
     plt.title("Line graph")
